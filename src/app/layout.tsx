5136cc8a8212998f61c64da4836010ef59a8c6d2
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-01-26 17:15:50 -0500

    Moved the header into a separate folder.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
 
-import { GithubIcon } from '@/components/icons/github-icon';
+import { Header } from '@/components/layout/header/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,24 +31,7 @@ const RootLayout: React.FC<{
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className='sticky top-0 z-10 h-10 w-full bg-white shadow-md'>
-          <nav className='flex h-full w-full justify-between gap-6 px-4'>
-            <div className='flex h-full items-center gap-4'>
-              <Link className='text-lg tracking-widest' href='/'>
-                QuickToolKit
-              </Link>
-            </div>
-            <div className='flex h-full items-center gap-4'>
-              <a
-                href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL ?? ''}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <GithubIcon size={24} />
-              </a>
-            </div>
-          </nav>
-        </header>
+        <Header />
         <main className='flex w-full items-center justify-center'>
           {children}
         </main>
