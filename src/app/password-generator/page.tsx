1f291eeabcec30ac7c3090d7cbaea64e710ebc6e
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-01-27 14:36:12 -0500

    Updated the copy on the password generator page.

--- src/app/password-generator/page.tsx
+++ src/app/password-generator/page.tsx
@@ -12,9 +12,8 @@ const Page: NextPage = () => {
     <div className='m-4 flex w-full max-w-2xl flex-col gap-4'>
       <h1 className='text-2xl font-bold'>Simple Password Generator</h1>
       <p className='text-sm text-gray-500'>
-        This is a simple, client-side random password generator. Your password
-        is generated on the client side and is not sent to the server. We will
-        not store your password or any other information.
+        This is a simple random password generator. Your password is generated
+        on the client side and will not be stored or sent to the server.
       </p>
       <PasswordGenerator />
     </div>
