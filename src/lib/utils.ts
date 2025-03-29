6999f41c93aef2eba7425688a57ece4f6df90834
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-03-29 18:20:55 -0400

    Upgraded tailwind to v4.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/** Replaces the existing filename extension of a file name. */
+export function replaceFileExtension(
+  filename: string,
+  filetype: string,
+): string {
+  if (filetype.startsWith('.')) {
+    filetype = filetype.substring(1);
+  }
+  const parts: string[] = filename.split('.');
+  if (parts.length > 1) {
+    parts.pop();
+    parts.push(filetype);
+  }
+  return parts.join('.');
 }
