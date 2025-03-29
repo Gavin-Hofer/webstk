48f5d68555bbb03e008017d56b0f3014bf1ad1e7
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-03-29 18:22:26 -0400

    Re-initialized shadcn.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-/** Replaces the existing filename extension of a file name. */
-export function replaceFileExtension(
-  filename: string,
-  filetype: string,
-): string {
-  if (filetype.startsWith('.')) {
-    filetype = filetype.substring(1);
-  }
-  const parts: string[] = filename.split('.');
-  if (parts.length > 1) {
-    parts.pop();
-    parts.push(filetype);
-  }
-  return parts.join('.');
+  return twMerge(clsx(inputs))
 }
