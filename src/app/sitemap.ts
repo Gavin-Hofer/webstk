74b371142f6e97450731219c203b6a69f35482c8
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-03-30 23:10:48 -0400

    Added image converter to the sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,24 @@
 import { MetadataRoute } from 'next';
 
+const CURRENT_DATE = new Date();
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://www.webstk.com',
-      lastModified: new Date(),
+      lastModified: CURRENT_DATE,
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: 'https://www.webstk.com/password-generator',
-      lastModified: new Date(),
+      lastModified: CURRENT_DATE,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.webstk.com/image-converter',
+      lastModified: CURRENT_DATE,
       changeFrequency: 'monthly',
       priority: 0.8,
     },
