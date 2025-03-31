3c5c6fbc2fda6b1c774c166fa7add621fb3bcbcf
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-03-30 20:19:58 -0400

    Removed an unused import.

--- src/app/image-converter/components/download-all-button.tsx
+++ src/app/image-converter/components/download-all-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { FileDownIcon } from 'lucide-react';
 
