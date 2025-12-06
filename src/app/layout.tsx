bdddd31a5d81cd12c24629ce16b12108cb7b5d0b
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-12-06 13:04:17 -0500

    feat: added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Analytics } from '@vercel/analytics';
 
 import { ReactQueryClientProvider } from '@/components/providers/react-query-client-provider';
 import { Header } from '@/components/layout/header/header';
@@ -55,6 +57,7 @@ const RootLayout: React.FC<{
         <Header />
         <main className='flex w-full items-center justify-center'>
           <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+          <Analytics />
         </main>
       </body>
     </html>
