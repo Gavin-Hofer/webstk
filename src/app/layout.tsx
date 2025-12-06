db5aaa9bde81f5550c68ec2771afb73d95d87ff4
Gavin Hofer <gavin.p.hofer@gmail.com>
2025-12-06 13:10:22 -0500

    feat: added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ReactQueryClientProvider } from '@/components/providers/react-query-client-provider';
 import { Header } from '@/components/layout/header/header';
