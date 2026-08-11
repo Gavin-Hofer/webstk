858023a2fdef51bb8fa55fbe3ebfa54afed896ac
Gavin Hofer <gavin.hofer@nordicsemi.no>
2026-08-11 11:40:41 -0400

    feat(sentry): set up sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://62895c1140633020d7c54b2de2a80fe0@o4511892867842048.ingest.us.sentry.io/4511892885864448',
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
